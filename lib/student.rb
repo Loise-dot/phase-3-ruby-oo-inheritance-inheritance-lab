a70f324382dc845479956d2d36076255768926d7
Loise <loise.wambui@student.moringaschool.com>
2023-02-17 00:37:22 +0300

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+class Student < User
+
+    # attr_accessor :knowledge
+    @knowledge = []
+
+    def initialize
+        @knowledge = []
+    end
+    
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+    def knowledge
+        @knowledge.map do |student_knowledge|
+            student_knowledge
+        end
+    end
 
 end
